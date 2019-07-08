489cd15ffbedc43e75a75303a24fc1e1fc30a826
Hasan Sa <hasansa007@gmail.com>
2019-07-09 02:25:59 +0300

    - update Package name - added ‘path’

--- Package.swift
+++ Package.swift
@@ -4,25 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-sdk",
+    name: "ApesterKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ios-sdk",
-            targets: ["ios-sdk"]),
+            name: "ApesterKit",
+            targets: ["ApesterKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ios-sdk",
-            dependencies: []),
+            name: "ApesterKit",
+            dependencies: [],
+            path: "Sources"),),
         .testTarget(
-            name: "ios-sdkTests",
-            dependencies: ["ios-sdk"]),
+            name: "ApesterKitTests",
+            dependencies: ["ApesterKit"]),
     ]
 )
