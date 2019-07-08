7fcfa0f59571dba23d4311cf23e75a8b6b0eb4a5
Hasan Sa <hasansa007@gmail.com>
2019-07-09 01:42:29 +0300

    - update Packge.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,25 @@
-//
-//  ApesterKit.swift
-//  ApesterKit
-//
-//  Created by Hasan Sa on 23/10/15.
-//  Copyright © 2017 Apester. All rights reserved.
-//
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ApesterKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ApesterKit",
+            targets: ["ApesterKit"]),
+    ],
     dependencies: [],
-    exclude: ["Tests"]
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ApesterKit",
+            dependencies: []),
+        .testTarget(
+            name: "ApesterKitTests",
+            dependencies: ["ApesterKit"]),
+    ]
 )
