4ea1aeddce810447874caa9b1363d7864729bc66
micky <michael.kroitoro@apester.com>
2024-02-28 12:23:15 +0200

    clean commit : podfile, and example app

---
+++ Example/ApesterKit/structs-old/enums.swift
@@ -0,0 +1,19 @@
+//
+//  enums.swift
+//  ApesterKit_Example
+//
+//  Created by Michael Krotorio on 1/25/24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+enum UnitType {
+    case Poll, Story,Quiz, Playlist
+}
+
+enum Category: String {
+    case lifestyle = "Lifestyle"
+    case news = "News"
+    case sports = "Sports"
+}
